f45679a2e5861b10450a5e983dbdc8364dcdf869
username <nkuluthuli@gmail.com>
2022-10-31 11:39:12 +0200

    Add playRound function to javascript.js and test functionality

--- javascript.js
+++ javascript.js
@@ -2,21 +2,34 @@ function getComputerChoice() {
     let choice = getRandomIntInclusive(1,3);
     console.log(choice)
     if (choice === 1) {
-        return "ROCK"
+        return "ROCK";
     }
     else if (choice === 2) {
-        return "PAPER"
+        return "PAPER";
     }
     else if (choice === 3) {
-        return "SCISSORS"
+        return "SCISSORS";
     }
 }
-
-console.log(getComputerChoice())
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
-  
+
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "ROCK" && computerSelection === "ROCK") {
+        return "It is a Draw! Both of you chose Rock";
+    }
+    else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "You Win! Rock beats Scissors";
+    }
+  }
+   
+  const playerSelection = "ROCK";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
