8153613511642e06f2ade92f1431c4ed728a7247
username <nkuluthuli@gmail.com>
2022-11-16 19:51:01 +0200

    Remove game function and add 3 buttons for player selection with event listeners

--- javascript.js
+++ javascript.js
@@ -48,40 +48,14 @@ function getRandomIntInclusive(min, max) {
     else return "Your choice is not valid";
   }
 
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerInput = (prompt("Please enter your choice between Rock Paper Sciccors")).toString();
-        let playerChoice = playerInput.toUpperCase();
-        let computerSelection = getComputerChoice();
-        let roundResult = playRound(playerChoice, computerSelection);
-        console.log(roundResult);
-        if (roundResult === "You Win! Rock beats Scissors" || 
-            roundResult === "You Win! Paper beats Rock" || 
-            roundResult === "You Win! Scissors beats Paper") {
-            playerScore++;
-        }
-        else if (roundResult === "You Lose! Paper beats Rock" || 
-                 roundResult === "You Lose! Scissors beats Paper" || 
-                 roundResult === "You Lose! Rock beats Scissors" || 
-                 roundResult === "Your choice is not valid") {
-                 computerScore++;
-        }
-        console.log(playerScore,computerScore);
-        console.log(playerChoice,computerSelection);
-     }
-     if (playerScore > computerScore) {
-        return "You Win!";
-     }
-     else if (computerScore > playerScore) {
-        return "You Lose!";
-     }
-     else if (playerScore === computerScore) {
-        return "It is a Draw!";
-     }
-}
-  
-console.log(game());
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        console.log(playRound(button.id, getComputerChoice()));
+      });
+    });
 
 
